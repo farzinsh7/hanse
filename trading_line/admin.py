559835219687c169acd4d5f9ae1c8ad9b95d1d9e
Your Namefarzinsh7 <you@example.com>
2024-01-09 09:48:10 +0330

    Fix bug in admin

--- trading_line/admin.py
+++ trading_line/admin.py
@@ -7,5 +7,5 @@ from . import models
 class TradingAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ["title"]}
     list_display = ('title', 'status')
-    list_filter = ('status', 'category')
+    list_filter = ('status',)
     search_fields = ('title', 'description')
