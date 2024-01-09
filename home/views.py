9d49eea1879e151c4fa51668d1e3af7dd3cf43e6
Your Namefarzinsh7 <you@example.com>
2024-01-09 10:19:59 +0330

    Dynamic sections in header & index

--- home/views.py
+++ home/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from .models import SiteInformation, HomeData
 from django.views.generic import ListView
+from trading_line.models import Trading
+from investment_line.models import Investment
 
 class Index(ListView):
     model = HomeData
     template_name = 'index.html'
     context_object_name = "home"
     queryset = HomeData.objects.first()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['trading'] = Trading.objects.published()[:3]
+        context['investment'] = Investment.objects.published()[:3]
+        return context
 
 
 
@@ -15,6 +23,12 @@ class SiteHeaderView(ListView):
     template_name = 'base/shared/header.html'
     context_object_name = 'info'
     queryset = SiteInformation.objects.first()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['trading'] = Trading.objects.published()[:5]
+        context['investment'] = Investment.objects.published()[:5]
+        return context
 
 
 class SiteFooterView(ListView):
