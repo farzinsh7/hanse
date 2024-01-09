117de9485bb21fb15555f40c487e20a8cfe7dc37
Your Namefarzinsh7 <you@example.com>
2024-01-09 12:36:48 +0330

    Fix gitignore & connect to mysqlclient

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hanse',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'F@rzin123',
     }
 }
 
