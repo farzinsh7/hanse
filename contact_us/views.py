ff5268ba535685a0c8f210969e4978ca0f820913
farzinsh7 <farzinnater@gmail.com>
2024-01-16 09:44:19 +0330

    Add email config to add-form

--- contact_us/views.py
+++ contact_us/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 from .models import ContactUS, ContactForm
 from django.http import HttpResponse
+from django.core.mail import send_mail
 
 
 class ContactUsView(ListView):
@@ -12,9 +13,21 @@ class ContactUsView(ListView):
     queryset = ContactUS.objects.first()
 
 def add_form(request):
-    name = request.POST.get('name')
-    email = request.POST.get('email')
-    phone = request.POST.get('phone')
-    message = request.POST.get('message')
-    form = ContactForm.objects.create(name=name, email=email, phone=phone, message=message)
-    return HttpResponse("<h5 class='color-success' >Your message has been successfully sent.</h5>")
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        form = ContactForm.objects.create(name=name, email=email, phone=phone, message=message)
+
+        # Send email notification to admin
+        subject = 'New Form Submission'
+        email_message = f"A new form submission:\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+        from_email = 'test@gravita.gold'  # Replace with your email
+        recipient_list = ['eg.styleofficial@gmail.com']  # Replace with your admin email address
+        send_mail(subject, email_message, from_email, recipient_list, fail_silently=False)
+
+        return HttpResponse("<h5 class='color-success'>Your message has been successfully sent.</h5>")
+
+    return HttpResponse("Invalid request method")
